cd3933faca293d7de870e122e44d6f678509fdda
wendy <5703@holbertonstudents.com>
2022-10-02 22:16:40 -0700

    Complete the source code in order to print whether the number stored in the variable n is positive or negative

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
-int main(void) {
+int main(void)
+{
 int n;
-n=98;	
-if (n> 0)
+if (n > 0)
 printf("%d  is positive", n);
 else if (n < 0)
 printf("-%d  is negative", n);
-else if (n==0)
-printf("0 is zero");
-return 0;
+else if (n == 0)
+printf("0 is zero", n);
+return (0);
 }
