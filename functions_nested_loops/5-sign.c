094d6bba8ddeadd6a2e49f299053b0a7c928ce98
wendy <5703@holbertonstudents.com>
2022-10-05 02:42:24 -0700

    Returns 1 and prints + if n is greater than zero, Returns 0 and prints 0 if n is zero, Returns -1 and prints - if n is less than zero

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -1,4 +1,3 @@
-#include <stdio.h>
 #include "main.h"
 /**
 * _isalpha - Entry point
@@ -10,17 +9,17 @@ int print_sign(int n)
 {
 	if (n > 0)
 	{
-		putchar('+');
+		_putchar('+');
 		return (1);
 	}
 	else if (n == 0)
 	{
-		putchar(0);
+		_putchar(0);
 		return (0);
 	}
 	else
 	{
-		putchar('-');
+		_putchar('-');
 		return (-1);
 	}
 return (0);
