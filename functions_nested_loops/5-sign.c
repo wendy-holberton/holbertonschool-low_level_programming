3c9b2f88b189a4d71aa41ae50b6d5221a4c05221
wendy <5703@holbertonstudents.com>
2022-10-05 02:29:46 -0700

    Returns 1 and prints + if n is greater than zero, Returns 0 and prints 0 if n is zero, Returns -1 and prints - if n is less than zero

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -10,17 +10,17 @@ int print_sign(int n)
 {
 	if (n > 0)
 	{
-		putchar('+1');
+		putchar('+');
 		return (1);
 	}
 	else if (n == 0)
 	{
-		putchar(00);
+		putchar(0);
 		return (0);
 	}
 	else
 	{
-		putchar('-1');
+		putchar('-');
 		return (-1);
 	}
 return (0);
