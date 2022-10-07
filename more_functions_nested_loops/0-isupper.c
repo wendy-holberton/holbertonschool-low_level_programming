cac9eec53869f01c20de2d2fe06918a447503cbb
wendy <5703@holbertonstudents.com>
2022-10-07 05:13:15 -0700

    Returns 1 if its uppercaes, otherwise returns 0

--- more_functions_nested_loops/0-isupper.c
+++ more_functions_nested_loops/0-isupper.c
@@ -1,16 +1,16 @@
 #include "main.h"
 /**
 * _isupper - Entry point
-* @r: The number to be collected
+* @c: The number to be collected
 * Return: Always 0 (Success)
 */
 int _isupper(int c)
 {
-	if ( c >= 65 && c<=90 )
+	if (c >= 65 && c <= 90)
 	{
 		return (1);
 	}
-	else if ( c >= 97 && c<=122 )
+	else if (c >= 97 && c <= 122)
 	{
 		return (0);
 	}
