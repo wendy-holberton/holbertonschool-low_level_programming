7b698baf73f0353cac473aebf3fb5947e51c5a31
wendy <5703@holbertonstudents.com>
2022-10-07 11:46:56 -0700

    Returns 1 if c is a digit, otherwise returns 0

---
+++ more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+* _isdigit - Entry point
+* @c: The number to be collected
+* Return: Always 0 (Success)
+*/
+int _isdigit(int c)
+{
+	if (c >= 48 && c <= 57)
+	{
+	return (1);
+	}
+	else 
+	{
+	return (0);
+	}
+return (0);
+}
