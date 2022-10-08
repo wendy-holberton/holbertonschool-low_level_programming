0d0ac816618d16f3015964c7d9e659dcf1359765
wendy <5703@holbertonstudents.com>
2022-10-07 23:36:17 -0700

    prints the numbers, from 0 to 9

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+* print_numbers - Entry point
+* @a: The number to be collected
+* Return: Always 0 (Success)
+*/
+void print_numbers(void)
+{
+	int a;
+
+	for (a = 48; a <= 57; a++)
+	{
+	_putchar(a);
+	}
+	_putchar('\n');
+}
