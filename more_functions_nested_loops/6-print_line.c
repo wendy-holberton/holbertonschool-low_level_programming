3e7f7c9f25d7661d13a899f6378f510be40db539
wendy <5703@holbertonstudents.com>
2022-10-09 04:21:15 -0700

    Where n is the number of times the character _ should be printed

--- more_functions_nested_loops/6-print_line.c
+++ more_functions_nested_loops/6-print_line.c
@@ -1,13 +1,13 @@
 #include "main.h"
 /**
 * print_line - Entry point
-* @a: The number to be collected
 * @n: The number of times
 * Return: Always 0 (Success)
 */
 void print_line(int n)
 {
 	char underscore = 95;
+	
 	while (n > 0)
 	{	
 	_putchar(underscore);
