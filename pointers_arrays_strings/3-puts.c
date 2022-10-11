3e385747bd1dee6e263de45e43ca3a76a589c213
wendy <5703@holbertonstudents.com>
2022-10-11 00:46:47 -0700

    prints a string to stdout

--- pointers_arrays_strings/3-puts.c
+++ pointers_arrays_strings/3-puts.c
@@ -2,8 +2,18 @@
 
 /**
  * _puts - puts the character
+ * @str - writes a string to stdout up to but not including null character.
  * Return: nothing.
  */
 void _puts(char *str)
 {
+	int a;
+
+	a = 0;
+	while (str[a] != '\0')
+	{
+		_putchar(str[a]);
+		a = a + 1;
+	}
 }
+
