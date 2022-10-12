5c614e8bfe9142007df5717f138e652190cba16f
wendy <5703@holbertonstudents.com>
2022-10-12 04:43:04 -0700

    reverse strings

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -2,8 +2,9 @@ void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 void _puts(char *str);
 int _strlen(char *s);
-int _putchar(char c);
 void print_rev(char *s);
 void rev_string(char *s);
-void puts2(char *str)
+void puts2(char *str);
+void puts_half(char *str);
 int _putchar(char c);
+char *_strcat(char *dest, char *src);
