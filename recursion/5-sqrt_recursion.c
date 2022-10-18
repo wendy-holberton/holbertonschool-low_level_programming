550e1b0e0dcf000678d3ca0aa8e420327b29fdac
wendy <5703@holbertonstudents.com>
2022-10-18 04:26:13 -0700

    return natural square root

---
+++ recursion/5-sqrt_recursion.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * _sqrt_recursion - square root recursion
+ * @n: n is the interger
+ * @current_number: is the interger
+ * Return: always 0
+ */
+int _sqrt_recur(int current_number, int n)
+{
+	if (current_number * current_number == n)
+	{
+		return (current_number);
+	}
+
+	if (current_number * current_number > n)
+	{
+		return (-1);
+	}
+	return (_sqrt_recur(current_number + 1, n));
+}
+int _sqrt_recursion(int n)
+{
+	return (_sqrt_recur(1, n));
+}
