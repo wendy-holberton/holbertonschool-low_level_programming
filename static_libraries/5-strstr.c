497d95956e8480ba3b4cc5c1037658166d3be031
wendy <5703@holbertonstudents.com>
2022-10-19 02:21:51 -0700

    add new

---
+++ static_libraries/5-strstr.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <string.h>
+/**
+ *_strstr - finds the first occurrence of the substring needle in the string haystack
+ * @haystack: This is the main C string to be scanned
+ * @needle: This is the small string to be searched with-in haystack string 
+ * return: 0
+ */
+char *_strstr(char *haystack, char *needle)
+{
+return (0);
+}
