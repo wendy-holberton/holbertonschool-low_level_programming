86d38b89643c063f280c2e58fe59839aeef7ebc2
wendy <5703@holbertonstudents.com>
2022-10-19 01:20:08 -0700

    add atoi

---
+++ static_libraries/100-atoi.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/**
+ * _atoi - changes the string argument str to an integer
+ */
+int _atoi(char *s)
+{
+printf("String value = %s\n", string2);
+printf("Integer value = %d\n", val);
+return (0);
+}
