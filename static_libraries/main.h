452bb1ec51835206df473c8d7df4abb2a3c59450
wendy <5703@holbertonstudents.com>
2022-10-19 19:39:03 -0700

    update top and bottom line

--- static_libraries/main.h
+++ static_libraries/main.h
@@ -1,3 +1,6 @@
+#ifndef _MAIN_H
+#define _MAIN_H 
+
 int _putchar(char c);
 int _islower(int c);
 int _isalpha(int c);
@@ -19,3 +22,5 @@ unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
 void fun(void);
+
+#endif
