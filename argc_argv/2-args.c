34d45d0e7dc9bf731028f62c4e94166e76b43825
wendy <5703@holbertonstudents.com>
2022-10-21 04:06:26 -0700

    print all arguments in separate line

---
+++ argc_argv/2-args.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+ *main - name
+ *@argc: count of the arguments
+ *@argv[]: 'an array of pointers to the string'
+ *i:
+ *Return: Always 0 (Success)
+ */
+
+
+int main(int argc, char **argv)
+{
+	int i;
+
+	i = 0;
+	while (i < argc)
+	{
+		printf("%s\n", argv[i]);
+		i = i + 1;
+	}
+return (0);
+}
+
