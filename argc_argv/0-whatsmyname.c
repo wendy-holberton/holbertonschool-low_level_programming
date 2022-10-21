8281d7121eae619cbc50e55648d0d86ef22e9f91
wendy <5703@holbertonstudents.com>
2022-10-20 17:29:00 -0700

    print the name

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+ * main - name
+ *@argc: count of the arguments 
+ *@argv[]: an array of pointers to the string
+ *return: 0
+ */
+
+
+int main(int argc, char *argv[])
+{
+	printf("mynameis\n");	
+	return (0);
+}
