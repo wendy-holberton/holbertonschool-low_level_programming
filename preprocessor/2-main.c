c125a00a90b1e6c112c8ba1d901b223c252c693b
wendy <5703@holbertonstudents.com>
2022-10-29 03:58:30 -0700

    print the name where the file was complied from

---
+++ preprocessor/2-main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - check the code
+ * Description: 'print the name where the file was complied from'
+ *
+ * Return: always 0 (Success)
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
