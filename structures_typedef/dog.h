2306ad28aaf03c60a8ed30fa34e0e8e2f5589828
wendy <5703@holbertonstudents.com>
2022-11-01 00:14:59 -0700

    define a new type dog_t as a new name

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -7,16 +7,15 @@
  * @owner: Third member
  * Description: Longer description
  */
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
+} dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
-
 
 
 #endif /* _MAIN_H */
