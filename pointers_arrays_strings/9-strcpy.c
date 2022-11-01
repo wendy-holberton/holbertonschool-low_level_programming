60dac76481344f490e6a56fd96bf2cf06f6972af
wendy <5703@holbertonstudents.com>
2022-11-01 03:21:23 -0700

    copy the string

--- pointers_arrays_strings/9-strcpy.c
+++ pointers_arrays_strings/9-strcpy.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _strcpy - copy the string 
+ * _strcpy - copy the string
  * @dest: - pointer to the destination array where the content is to be copied
  * @src: - string which will be copied
  * Return: the pointer to dest.
@@ -16,7 +16,7 @@ char *_strcpy(char *dest, char *src)
 		dest[i] = src[i];
 		i = i + 1;
 	}
-	dest[i] = '\0'; 
+	dest[i] = '\0';
 	return (dest);
 
 }
