1327a7f19d8e29c338bbd4e79ca384f87f243427
wendy <5703@holbertonstudents.com>
2022-10-31 20:35:14 -0700

    updates

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -14,4 +14,14 @@ struct dog
 	char *owner;
 };
 
-#endif /* _DOG_H */
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+/**
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+*/
+
+
+
+#endif /* _MAIN_H */
+
