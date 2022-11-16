7e13df83728e7ccb95306a98b8fe6e91a8f76158
wendy <5703@holbertonstudents.com>
2022-11-16 01:25:42 -0800

    print all elements

--- more_singly_linked_lists/lists.h
+++ more_singly_linked_lists/lists.h
@@ -7,12 +7,11 @@
  * @next: points to the next node
  *
  * Description: singly linked list node structure
- * 
  */
 typedef struct listint_s
 {
-    int n;
-    struct listint_s *next;
+	int n;
+	struct listint_s *next;
 } listint_t;
 
 size_t print_listint(const listint_t *h);
@@ -27,5 +26,4 @@ int sum_listint(listint_t *head);
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n);
 int delete_nodeint_at_index(listint_t **head, unsigned int index);
 
-#endif /* __LISTS_H__ */	
-	
+#endif /* __LISTS_H__ */
