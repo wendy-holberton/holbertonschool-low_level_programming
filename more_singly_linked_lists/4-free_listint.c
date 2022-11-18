8d61f48ca8cb3836c1fb1dd2ee4b22d4e21aeffc
wendy <5703@holbertonstudents.com>
2022-11-17 21:39:05 -0800

    free a list

--- more_singly_linked_lists/4-free_listint.c
+++ more_singly_linked_lists/4-free_listint.c
@@ -9,9 +9,11 @@
  */
 void free_listint(listint_t *head)
 {	listint_t *ptr;
-
-	free(head->next);
-	ptr = head->next;
-	free(head);
-	head = ptr;
+	
+	while (head != NULL)
+	{
+		ptr = head->next;
+		free(head);
+		head = ptr;
+	}
 }
