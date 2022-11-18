11412050b035533de897e4812329c6df29c9a4e3
wendy <5703@holbertonstudents.com>
2022-11-17 21:28:05 -0800

    free a list

---
+++ more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,17 @@
+#include <stddef.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * free_listint - free a list
+ * @head: a pointer to listint_t
+ * Return: nothing
+ */
+void free_listint(listint_t *head)
+{	listint_t *ptr;
+
+	free(head->next);
+	ptr = head->next;
+	free(head);
+	head = ptr;
+}
