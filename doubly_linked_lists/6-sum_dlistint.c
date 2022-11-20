0f39b30c74137d9a702548011680ffe54003fb78
wendy <5703@holbertonstudents.com>
2022-11-19 19:02:40 -0800

    sum all data in a list

---
+++ doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * sum_dlistint - sum all the data in list
+ * @head: a pointer to dlistint_t
+ *
+ * Return: EXIT_SUCCESS, or 0 if list is empty
+ */
+int sum_dlistint(dlistint_t *head)
+{
+	int sum;
+
+	sum = 0;
+	while (head != NULL)
+	{
+			sum = sum + head->n;
+			head = head->next;
+	}
+	return (sum);
+}
