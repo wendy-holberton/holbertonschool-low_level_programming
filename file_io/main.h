5be419d19b39009ab838fbe3f95e202ade3265a4
wendy <5703@holbertonstudents.com>
2022-11-22 21:01:03 -0800

    added main

--- file_io/main.h
+++ file_io/main.h
@@ -1,5 +1,14 @@
 #ifndef __MAIN_H__
 #define __MAIN_H__
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+
+
 
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
