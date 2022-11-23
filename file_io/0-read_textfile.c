5b2bacd912213aa2e41c55adafe9eebacfffae28
wendy <5703@holbertonstudents.com>
2022-11-23 02:46:19 -0800

    read a text file and print it

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -1,7 +1,7 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
-#include <unistd.h> 
+#include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include "main.h"
@@ -10,10 +10,7 @@
  * read_textfile - read a text file
  * @filename: a pointer to const char *
  * @letters: the number of letters
- * Return: the acutal number of letters; 
-           or 0 if file cannot be opened / read;
-	   or 0 if filename is NULL;
-	   or 0 if write fails / not write expected amount of bytes  
+ * Return: wr or 0L
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
@@ -21,8 +18,8 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	int fd, rd, wr;
 
 	if (filename == NULL)
- 	{
- 		return (0);
+	{
+		return (0);
 	}
 
 	buff = malloc(sizeof(*buff) * letters);
@@ -30,14 +27,14 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	{
 		return (0);
 	}
-	
+
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
 	{
 		free(buff);
 		return (0);
 	}
-	
+
 	rd = read(fd, buff, letters);
 	if (rd == -1)
 	{
