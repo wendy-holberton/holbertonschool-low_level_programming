dd6a1a49a65dc2fadd72720abf7d5f8ad0aa054d
wendy <5703@holbertonstudents.com>
2022-11-22 20:59:55 -0800

    create a file

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -38,11 +38,11 @@ int create_file(const char *filename, char *text_content)
 	{
 		return (-1);
 	}
-	write_text = write (fd, text_content, strlen(text_content));
-	if (write_text == -1)
+	wr= write(fd, text_content, strlen(text_content));
+	if (wr == -1)
 	{
 		close (fd);
-		return (-1);
+		return (1);
 	}	
 	close (fd);
 	return (1);
