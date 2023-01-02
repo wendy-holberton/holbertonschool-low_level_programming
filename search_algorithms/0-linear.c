3542e57d00884f4fc23e1fbcfecefd06d4807e86
wendy <5703@holbertonstudents.com>
2023-01-02 03:50:16 -0800

    add linear search, h and README file

---
+++ search_algorithms/0-linear.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include "search_algos.h"
+
+/**
+ * linear_search - linear search for a value in an array of integers
+ *
+ * @array: a pointer to the first element of the array
+ * @size: the number of elements in array
+ * @value: the value to serach for
+ * Return: -1 if array is NULL or not found value; or return i
+ */
+int linear_search(int *array, size_t size, int value)
+{
+	size_t i;
+
+	i = 0;
+	while (i < size)
+	{
+		if (array[i] == value)
+		{
+			return (i);
+		}
+		i = i + 1;
+	}
+	if (array == NULL)
+	{
+		return (-1);
+	}
+	return (-1);
+}
---
+++ search_algorithms/search_algos.h
@@ -0,0 +1,15 @@
+#ifndef __search_algos_h__
+#define __search_algos_h__
+
+
+int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+
+
+
+
+
+
+
+
+#endif /* __search_algos_h__ */
