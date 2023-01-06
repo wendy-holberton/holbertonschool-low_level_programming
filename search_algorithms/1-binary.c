6258565586be5e041f36e781858b69f5c70743fb
wendy <5703@holbertonstudents.com>
2023-01-06 04:17:56 -0800

    added print_array function

--- search_algorithms/1-binary.c
+++ search_algorithms/1-binary.c
@@ -1,8 +1,32 @@
 #include <stddef.h>
 #include <stdio.h>
 #include "search_algos.h"
+
 /**
- * binary_search - searches for a value in a sorted array of integers 
+ * print_array - print the array
+ *
+ * @array: a pointer to the first element of the array
+ * @leftIndex: the first index in the array
+ * @rightIndex: the last index in the array
+ */
+void print_array(int *array, size_t leftIndex, size_t rightIndex)
+{
+	size_t index;
+
+	index = leftIndex;
+
+	printf("Searching in array:");
+
+	while (index < rightIndex)
+	{
+		printf(" %d,", array[index]);
+		index = index + 1;
+	}
+	printf(" %d\n", array[rightIndex]);
+}
+
+/**
+ * binary_search - searches for a value in a sorted array of integers
  *
  * @array: a pointer to the first element of the array to search in
  * @size: the number of elements in array
@@ -13,63 +37,40 @@ int binary_search(int *array, size_t size, int value)
 {
 	size_t leftIndex, rightIndex;
 	int middlePoint;
-	
+
 	leftIndex = 0;
 	rightIndex = size - 1;
-		
+
 	if (leftIndex > rightIndex)
 	{
 		return (-1);
 	}
-
+	if (array[leftIndex] == value)
+	{
+		return (-1);
+	}
 	while (leftIndex <= rightIndex)
 	{
+		print_array(array, leftIndex, rightIndex);
 		middlePoint = leftIndex + (rightIndex - leftIndex) / 2;
 
 		if (array[middlePoint] < value)
 		{
 			leftIndex = middlePoint + 1;
 		}
-		
 		else if (array[middlePoint] > value)
 		{
 			rightIndex = middlePoint - 1;
 		}
-	
+
 		else
 		{
 			return (middlePoint);
 		}
-	 	printf("Searching in array: %d, %d, %d\n", array[leftIndex], array[middlePoint], array[rightIndex]);	
 	}
-/**		printf("Searching in array: %d, %d, %d\n", array[leftIndex], array[middlePoint], array[rightIndex]);*/
-/**		printf("Searching in array: %d\n", array[leftIndex]);*/
-/**		printf("Searching in array: %d\n", array[rightIndex]);*/
-		
-	return (-1);
-}
-
-/**
-	while (leftIndex == rightIndex)
+	if (array == NULL)
 	{
-		middlePoint = leftIndex + (rightIndex - leftIndex) / 2;
-		
-		if (array[middlePoint] > value)
-		{
-			rightIndex = middlePoint - 1;
-		}
-		
-		else
-		{
-			leftIndex = middlePoint;
-		}
-		printf("leftIndex %lu, rightIndex %lu, middlePoint %d\n", leftIndex, rightIndex, middlePoint);
-	}
-	if (array[leftIndex] == value)
-	{
-		return (leftIndex);
+		return (-1);
 	}
 	return (-1);
 }
-
-*/
